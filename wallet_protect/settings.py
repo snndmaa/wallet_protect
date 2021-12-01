8640ed32504d9b4dc8c746eddb085360af9f82b2
sndmaa <sanclaw55@gmail.com>
2021-12-01 10:34:43 +0100

    django-heroku add

--- wallet_protect/settings.py
+++ wallet_protect/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/secure/showoga'
 LOGOUT_REDIRECT_URL = ''
+
+django_heroku.settings(locals())
