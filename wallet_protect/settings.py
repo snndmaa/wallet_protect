b8a94ecbf917adb57df746f2ae03c360dc1a83cc
sndmaa <sanclaw55@gmail.com>
2021-12-01 11:34:49 +0100

    https force

--- wallet_protect/settings.py
+++ wallet_protect/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('debug_val')
+# DEBUG = os.environ.get('debug_val')
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['*']
 
